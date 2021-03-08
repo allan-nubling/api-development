40323ce6002407a816b63d7d609d46adec35546a
Allan Nubling <nubling@gmail.com>
2021-03-07 23:38:47 -0300

    setting up env and development paths

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import app from '@config/app'
 const { PORT } = process.env
 
 app.listen(PORT, () => {
-    console.log(`⚡️[mailer-server]: Server is running at http://localhost:${PORT}`)
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
 })
